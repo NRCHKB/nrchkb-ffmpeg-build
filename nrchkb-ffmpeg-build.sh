bc6338f20440d74c53f8b2d09feac3ee160cb97c
Marcus Davies <55892693+marcus-j-davies@users.noreply.github.com>
2022-02-18 09:28:16 +0000

    add nasm

--- nrchkb-ffmpeg-build.sh
+++ nrchkb-ffmpeg-build.sh
@@ -82,7 +82,7 @@ installDependencies() {
     echo " |                                                       |"
     echo " ---------------------------------------------------------"
     echo
-    sudo apt install -y pkg-config autoconf automake libtool git wget make g++
+    sudo apt install -y pkg-config autoconf automake libtool git wget make g++ nasm
 
     LibXCheck=(sudo apt info libx264-dev)
 
