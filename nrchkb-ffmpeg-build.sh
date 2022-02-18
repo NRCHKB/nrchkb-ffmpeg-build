f399604c253787305e26779bcc659c178d3a17a8
crxporter <38265886+crxporter@users.noreply.github.com>
2022-02-18 12:48:05 -0600

    typo

--- nrchkb-ffmpeg-build.sh
+++ nrchkb-ffmpeg-build.sh
@@ -7,7 +7,7 @@
 # Copyright (c) 2022 Marcus Davies
 # Copyright (c) 2022 Garrett Porter
 
-# Peission is hereby granted, free of charge, to any person obtaining a copy
+# Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
