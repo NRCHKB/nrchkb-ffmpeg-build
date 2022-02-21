3edb16e297c747bc112cbcba6f6b45cfdf00ab5f
Marcus Davies <55892693+marcus-j-davies@users.noreply.github.com>
2022-02-21 21:57:04 +0000

    Update build-bootstrap.sh

--- build-bootstrap.sh
+++ build-bootstrap.sh
@@ -7,9 +7,9 @@ End=$'\e[0m'
 
 printf "\033c"
 
-if [[ which apt ]]; then
+if [[ `which apt` ]]; then
    bash <(curl -sL https://raw.githubusercontent.com/NRCHKB/nrchkb-ffmpeg-build/Alpine-Support/build-ubuntu.sh)
-elif [[ which apk ]]; then
+elif [[ `which apk` ]]; then
    bash <(curl -sL https://raw.githubusercontent.com/NRCHKB/nrchkb-ffmpeg-build/Alpine-Support/build-alpine.sh)
 else
    echo " ${Red}Sorry! - Your OS does not me the requirements for this script.${End}"
