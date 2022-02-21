d8e3f854bc35208c7c91efd5af701e4821e65464
Marcus Davies <55892693+marcus-j-davies@users.noreply.github.com>
2022-02-21 21:48:58 +0000

    Create build-bootstrap.sh

---
+++ build-bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Colors
+Red=$'\e[0;31m'
+Yellow=$'\e[1;33m'
+End=$'\e[0m'
+
+printf "\033c"
+
+if [[ 'which apt` ]]; then
+   bash <(curl -sL https://raw.githubusercontent.com/NRCHKB/nrchkb-ffmpeg-build/main/ubuntu.sh)
+elif [[ `which apk` ]]; then
+   bash <(curl -sL https://raw.githubusercontent.com/NRCHKB/nrchkb-ffmpeg-build/main/alpine.sh)
+else
+   echo " ${Red}Sorry! ${End}"
+fi
+
