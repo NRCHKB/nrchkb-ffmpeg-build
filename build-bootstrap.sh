f4cc9d85dab2b4a054d6e7bbc3bc787393a00b6f
Marcus Davies <55892693+marcus-j-davies@users.noreply.github.com>
2022-04-11 18:01:03 +0100

    Update build-bootstrap.sh

--- build-bootstrap.sh
+++ build-bootstrap.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+# Override Branch?
+if [[ -z "${BRANCH_NAME}" ]]; then
+  BRANCH_NAME="main"
+fi
+
 # Clear Terminal
 printf "\033c"
 
 # OSX
 if [[ "$OSTYPE" == "darwin"* ]]; then
    if [[ $(which brew) ]]; then
-      bash <(curl -sL https://raw.githubusercontent.com/NRCHKB/nrchkb-ffmpeg-build/main/build-osx.sh) "$@"
+      bash <(curl -sL https://raw.githubusercontent.com/NRCHKB/nrchkb-ffmpeg-build/$BRANCH_NAME/build-osx.sh) "$@"
    else
       echo
       echo " ----------------------------------------------------------------------------------------------------"
@@ -18,10 +23,10 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
    exit 0
 # Apline
 elif [[ $(which apk) ]]; then
-   bash <(curl -sL https://raw.githubusercontent.com/NRCHKB/nrchkb-ffmpeg-build/main/build-alpine.sh) "$@"
+   bash <(curl -sL https://raw.githubusercontent.com/NRCHKB/nrchkb-ffmpeg-build/$BRANCH_NAME/build-alpine.sh) "$@"
    exit 0
 # Debian
 elif [[ $(which apt) ]]; then
-   bash <(curl -sL https://raw.githubusercontent.com/NRCHKB/nrchkb-ffmpeg-build/main/build-debian.sh) "$@"
+   bash <(curl -sL https://raw.githubusercontent.com/NRCHKB/nrchkb-ffmpeg-build/$BRANCH_NAME/build-debian.sh) "$@"
    exit 0
 fi
